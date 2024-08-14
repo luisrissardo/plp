68186f0a25a85c484cd0e140cf7f9996eee0297f
luisrissardo <134444834+luisrissardo@users.noreply.github.com>
2024-08-13 21:43:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let body = document.querySelector('Body');
+let header = document.querySelector('Header');
+let nav = document.querySelector('Nav');
+
+window.addEventListener('scroll', ()=>{
+    nav.classList.toggle('scroll', )
+})
